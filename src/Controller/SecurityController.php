367965a5b63a22b6972d937a100b8474e7f556ba
unknown <you@example.comghislain.brice.fohom@esigetel.net>
2021-03-29 14:09:26 +0200

    third commit

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,6 +14,7 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        dd(2);
          if ($this->getUser()) {
              return $this->redirectToRoute('account');
          }
