5cba273c6880d79ff5ddc3ab5d77e00262a4b8f0
unknown <you@example.comghislain.brice.fohom@esigetel.net>
2021-03-29 13:50:48 +0200

    fourth commit

--- migrations/Version20210329114914.php
+++ migrations/Version20210329114914.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210326074250 extends AbstractMigration
+final class Version20210329114914 extends AbstractMigration
 {
     public function getDescription() : string
     {
