d0fd2eadcfccbe61050c22ed815f71aed9282387
unknown <you@example.comghislain.brice.fohom@esigetel.net>
2021-04-10 19:26:36 +0200

    check

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -20,7 +20,7 @@ class StripeController extends AbstractController
     {
         
         $product_for_stripe=[];
-        $YOUR_DOMAIN = 'https:/www.djsem-electronic.com';
+        $YOUR_DOMAIN = 'https://www.djsem-electronic.com';
         
         $order= $entityManager
         ->getRepository(Order::class)
