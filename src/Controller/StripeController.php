439f9d27ddb10c79903d1251a357283bdf125cf1
unknown <you@example.comghislain.brice.fohom@esigetel.net>
2021-04-15 09:25:31 +0200

    a

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -65,7 +65,17 @@ class StripeController extends AbstractController
 
 
         //
+            $checkout_session = Session::create([
+            //'customer_email'=>$this->getUser()->getEmail(),
+            'payment_method_types' => ['card'],
 
+            'line_items' => [
+                $product_for_stripe
+            ],
+            'mode' => 'payment',
+            //'success_url' => $YOUR_DOMAIN . '/commande/merci/{CHECKOUT_SESSION_ID}',
+            //'cancel_url' => $YOUR_DOMAIN . '/commande/erreur/{CHECKOUT_SESSION_ID}',
+            ]);
         dd($product_for_stripe);
 
         $order->setStripeSessionId($checkout_session->id);
